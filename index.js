aaf9d59572d8e72ef1203d68b8766497e738ac3d
Daniel Weigel <daniel.wgel@gmail.com>
2021-09-15 08:19:49 +1200

    Callback added to watch for events start, tick and complete

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 class Timer{
-    constructor(durationInput,startButton,pauseButton){
+    constructor(durationInput,startButton,pauseButton,callbacks){
         this.durationInput=durationInput;
         this.startButton=startButton;
         this.pauseButton=pauseButton;
+
+        if(callbacks){
+            this.onStart=callbacks.onStart;
+            this.onTick=callbacks.onTick;
+            this.onComplete=callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click',this.start);
         this.pauseButton.addEventListener('click',this.pause);
     }
     start=()=>{
+        if(this.onStart){
+            this.onStart();
+        }
         this.tick();
         this.timer=setInterval(this.tick,1000);
     }
     tick=()=>{
+        
       
-        this.timeRemaining=this.timeRemaining-1;
+        
+        if(this.timeRemaining<=0){
+            this.pause();
+            if(this.onComplete){
+                this.onComplete();
+            }
+        }else{
+            this.timeRemaining=this.timeRemaining-1;
+            if(this.onTick){
+                this.onTick();
+            }
+        }
         
     }
     get timeRemaining(){
@@ -35,6 +56,16 @@ const startButton=document.querySelector('#start');
 const pauseButton=document.querySelector('#pause');
 
 
-const timer=new Timer(durationInput,startButton,pauseButton);
+const timer=new Timer(durationInput,startButton,pauseButton,{
+    onStart(){
+        console.log('Timer started');
+    },
+    onTick(){
+        console.log('Timer just ticked down');
+    },
+    onComplete(){
+        console.log('Time completed');
+    }
 
+});
 // timer.start();
